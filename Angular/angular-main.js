0052d2c764b5a57b7b235a09d390551ce65ce989
AlexCruzeiro <alexcruzeiro@rocketmail.com>
2016-09-30 16:49:08 -0300

    Criação da primeira página dinâmica

---
+++ Angular/angular-main.js
@@ -0,0 +1,14 @@
+angular.module("prototipo", ["ngRoute"]); //Definindo o modulo prototipo
+angular.module("prototipo").config(function($routeProvider){ //Configurando o controlador de fluxo das páginas
+    $routeProvider.when("/home",        //Ligando a referência ao html de visão
+    {
+        templateUrl: "Angular/homeView.html"
+    });
+
+    $routeProvider.when("/teste",
+    {
+        templateUrl: "Angular/testeView.html"
+    });
+
+    $routeProvider.otherwise({redirectTo: "/home"});
+});

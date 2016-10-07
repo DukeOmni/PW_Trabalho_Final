9b7cdafce2a7be9cfee9a3d7eddfad12bc0f33e3
AlexCruzeiro <alexcruzeiro@rocketmail.com>
2016-10-07 12:28:10 -0300

    Adição da dashboard lateral

--- prototipo/src/main/webapp/Angular/angular-main.js
+++ prototipo/src/main/webapp/Angular/angular-main.js
@@ -2,21 +2,21 @@ angular.module("prototipo", ["ngRoute"]); //Definindo o modulo prototipo
 angular.module("prototipo").config(function($routeProvider){ //Configurando o controlador de fluxo das páginas
     $routeProvider.when("/home",        //Ligando a referência ao html de visão
     {
-        templateUrl: "Angular/homeView.html"
+        templateUrl: "view/home.html"
     });
 
     $routeProvider.when("/teste",
     {
-        templateUrl: "Angular/testeView.html"
+        templateUrl: "view/teste.html"
     });
 
     $routeProvider.when("/RelatorioDeVenda",
     {
-        templateUrl: "Angular/relatorio_de_vendaView.html"
+        templateUrl: "view/relatorio_de_venda.html"
     });
     
     $routeProvider.otherwise({redirectTo: "/home"});
 	$routeProvider.when("/cadastro",
-	{ templateUrl: "Angular/view_cadastro.html"
+	{ templateUrl: "view/cadastro.html"
 });
 });

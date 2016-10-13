e5e8750e7d2982e98fb80aac21cfbc6e6c6b2832
AlexCruzeiro <alexcruzeiro@rocketmail.com>
2016-10-12 23:03:10 -0300

    Criação do conteúdo da página cadastro e modificação da tabela de relatório na página relatório de vendas

--- prototipo/src/main/webapp/app1.js
+++
@@ -1,2 +0,0 @@
-var app1 = angular.module("app1",["ngAnimate"]);
-var app2 = angular.module("app2",["ngRoute"]);
--- prototipo/src/main/webapp/controllers/relatorio_de_vendas.js
+++ prototipo/src/main/webapp/controllers/relatorio_de_vendas.js
@@ -1,10 +1,35 @@
 SR
 .controller("relatorioCtrl", function ($scope) {
+    var alturaDiv = screen.height - 100;
+    $scope.altura = alturaDiv;
     $scope.veiculos = [{
         veiculo: "carro1",
         vendedor: "vendedor1",
         cliente: "cliente1",
         FP: "FP1",
         valor: "valor1"
+    },
+        {
+        veiculo: "carro2",
+        vendedor: "vendedor2",
+        cliente: "cliente2",
+        FP: "FP2",
+        valor: "valor2"
+    },
+        {
+        veiculo: "carro3",
+        vendedor: "vendedor4",
+        cliente: "cliente4",
+        FP: "FP4",
+        valor: "valor4"
+    },
+        {
+        veiculo: "carro5",
+        vendedor: "vendedor5",
+        cliente: "cliente5",
+        FP: "FP5",
+        valor: "valor5"
     }];
 });
+
+
